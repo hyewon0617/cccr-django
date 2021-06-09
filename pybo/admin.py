89d1d0832d022811f8844a5bfb9745d0e4ac98ee
hyewon <hyewon@git.co.kr>
2021-06-09 13:40:15 +0900

    admin

--- pybo/admin.py
+++ pybo/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Question
 
-# Register your models here.
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    search_fields=['subject','content']
+    list_display=['id','subject','short_content','create_date']
+    def short_content(self, item):
+        return item.content[:10]
+
+        
