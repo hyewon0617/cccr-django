0fcfd0659a87d9b489fbdd5ac013b878a60d5662
hyewon <hyewon@git.co.kr>
2021-06-09 15:06:29 +0900

    add detail

--- pybo/views.py
+++ pybo/views.py
@@ -1,5 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
+from .models import Question
+
 
 def index(request):
-    return HttpResponse("hello world")
+    # pybo list
+    question_list = Question.objects.order_by('-create_date')
+    context = {'question_list': question_list}
+
+    return render(request, 'pybo/question_list.html', context)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
+    return render(request, 'pybo/question_detail.html', context)
