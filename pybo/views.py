9973703375dcdb4aa1059fe0bfa7e1ce36dd210b
hyewon <hyewon@git.co.kr>
2021-06-10 17:03:45 +0900

    add templatetags-filter

--- pybo/views.py
+++ pybo/views.py
@@ -1,15 +1,23 @@
+from django.core import paginator
 from django.shortcuts import get_object_or_404, redirect, render
 from django.http import HttpResponse
 from .models import Question, Answer
 from django.utils import timezone
 from .forms import QuestionForm, AnswerForm
+from django.core.paginator import Paginator
 
 
 def index(request):
     # pybo list
+    page = request.GET.get('page', '1')
     question_list = Question.objects.order_by('-create_date')
-    context = {'question_list': question_list}
+    
+    paginator = Paginator(question_list, 10)
+    page_obj = paginator.get_page(page)
 
+    context = {'question_list': page_obj}
+
+    
     return render(request, 'pybo/question_list.html', context)
 
 
